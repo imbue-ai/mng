49bcb181e8af3b928e89d038bc04eef1973f2659
Josh Albrecht <thejash@gmail.com>
2026-01-26 20:01:28 -0800

    Fix tests

--- libs/mngr/imbue/mngr/providers/modal/routes/example.py
+++ libs/mngr/imbue/mngr/providers/modal/routes/example.py
@@ -6,7 +6,8 @@ app = modal.App(name="mngr-8caed3bc40df435fae5817ea0afdbf77-modal", image=image)
 
 @app.function()
 @modal.fastapi_endpoint(
-    docs=True  # adds interactive documentation in the browser
+    # adds interactive documentation in the browser
+    docs=True
 )
 def hello():
     return "Hello world!"
