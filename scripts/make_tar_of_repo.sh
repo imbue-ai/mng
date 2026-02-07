ee49a16ac4eeffd9ec1f156462c809e40020cf99
Josh Albrecht <thejash@gmail.com>
2026-02-07 09:35:46 -0800

    Trying to make a working configuration for remote modal agents

--- scripts/make_tar_of_repo.sh
+++ scripts/make_tar_of_repo.sh
@@ -9,9 +9,10 @@ mkdir -p "$DEST";
 [ -e "$DEST/$HASH.tar.gz" ] || ( \
   tmp=$(mktemp -d); \
   rm -rf "$tmp"; \
-  real_origin=$(git remote get-url origin); \
+  real_origin="https://github.com/$(git remote get-url origin | sed 's|.*github.com[:/]||')"; \
   git clone . "$tmp"; \
   git -C "$tmp" remote set-url origin "$real_origin"; \
+  git -C "$tmp" fetch --all; \
   git -C "$tmp" checkout "$HASH"; \
   mv "$tmp" "$DEST/$HASH"; \
   tar czf "$DEST/current.tar.gz" -C "$DEST/$HASH" .; \
