bb819430d2481328b6cebe9ee40d567d0dee6e67
Josh Albrecht <joshalbrecht@gmail.com>
2026-03-06 11:03:10 -0800

    Decrease timeout

--- libs/mng_claude_changeling/imbue/mng_claude_changeling/plugin.py
+++ libs/mng_claude_changeling/imbue/mng_claude_changeling/plugin.py
@@ -129,7 +129,7 @@ class ClaudeChangelingAgent(ClaudeAgent):
 
     enter_submission_timeout_seconds: float = Field(
         # increased timeout because we don't want to send duplicate events if we can avoid it
-        default=2 * 60.0,
+        default=60.0,
         description="Timeout in seconds for waiting on the enter submission signal",
     )
 
